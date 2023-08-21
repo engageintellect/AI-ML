3a1d9e55acc5dffc1fe6efdd86e540b9383c7f56
engageintellect <jc9361@gmail.com>
2023-08-20 19:57:23 -0700

    adding stable diffusion

---
+++ huggingface/stable-diffusion/main.py
@@ -0,0 +1,13 @@
+from diffusers import StableDiffusionPipeline
+import torch
+
+# TODO: not working in wsl2. test on osx
+
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]  
+    
+image.save("astronaut_rides_horse.png")
